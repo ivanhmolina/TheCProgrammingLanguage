36b4141401442b453f23a4a5e424cfe910fd065a
ivanhmolina <35584318+ivanhmolina@users.noreply.github.com>
2021-09-01 19:45:00 -0500

    EXPERIMENTS - ACCESING ACCESSING BIT STRUCTURE UPDATE
    set_port added
    initialize_ports updated
    read_port updated

--- Experiments/accessing_bit_structure.c
+++ Experiments/accessing_bit_structure.c
@@ -5,7 +5,7 @@
 #define NUM_PORTS 8U
 
 typedef unsigned short int uint16_t;
-typedef enum STATUS_E {PASS, ERROR} STATUS_T;
+typedef enum STATUS_E {PASS, FAIL} STATUS_T;
 
 typedef struct PORTS_S
 {
@@ -21,17 +21,93 @@ typedef struct PORTS_S
 }PORTS_T;
 
 static void initialize_ports(PORTS_T ** uports);
+static void print_ports(PORTS_T *pports); 
 static STATUS_T read_port(uint16_t port, PORTS_T *pports, uint16_t *port_active);
+static STATUS_T set_port(uint16_t port, PORTS_T *pports, bool status);
+
 
 int main()
 {
-    STATUS_T success = ERROR;
+    STATUS_T success = FAIL;
     PORTS_T ports;
     PORTS_T *pports = &ports;
-    uint16_t port_status = 0U;
-    uint16_t port_number = 0U;
     
     initialize_ports(&pports);
+    print_ports(pports);
+    set_port(1, pports, true);
+    set_port(6, pports, true);
+    print_ports(pports);
+    set_port(1, pports, false);
+    print_ports(pports);
+}
+
+static void initialize_ports(PORTS_T ** uports)
+{
+    STATUS_T result = PASS;
+
+    (*uports)->reserved = 1U;
+    
+    for (uint16_t port = 1; port <= NUM_PORTS; port++)
+    {
+        result += set_port(port, (*uports), false);    
+    }
+    if(result != PASS)
+    {
+        printf("Something went wrong in initialization...\n");
+    }
+}
+
+static STATUS_T set_port(uint16_t port, PORTS_T *pports, bool status)
+{
+    STATUS_T result = PASS;
+    uint16_t temp_ports = 0U;
+
+    if ((port > 0U) && (port <= NUM_PORTS))
+    {
+        memcpy(&temp_ports, pports, sizeof(temp_ports));
+        if(true == status)
+        {
+            temp_ports = temp_ports | ((uint16_t)1U << (port - 1U));
+        }
+        else
+        {
+            temp_ports = temp_ports & (~((uint16_t)1U << (port - 1U)));
+        }
+        memcpy(pports, &temp_ports, sizeof(temp_ports));
+    }
+    else
+    {
+        result = FAIL;
+    }
+
+    return result;
+}
+
+static STATUS_T read_port(uint16_t port, PORTS_T *pports, uint16_t *port_active)
+{
+    STATUS_T result = PASS;
+    uint16_t temp_ports = 0U;
+
+    if((port > 0U) && (port <= NUM_PORTS))
+    {
+        memcpy(&temp_ports, pports, sizeof(temp_ports));
+        *port_active = (temp_ports & ((uint16_t)1U << (port - 1U))) >> (port - 1U);
+    }
+    else
+    {
+        result = FAIL;
+    }
+
+    return result;
+}
+
+static void print_ports(PORTS_T *pports)
+{
+    uint16_t port_status = 0U;
+    uint16_t port_number = 0U;
+    STATUS_T success = FAIL;
+
+    printf("PORT LINKS STATUS\n");
 
     for (port_number = 1; port_number <= NUM_PORTS; port_number++)
     {
@@ -42,34 +118,7 @@ int main()
         }
         else
         {
-            printf("There was an error");
+            printf("There was an error\n");
         }
     }
-}
-
-static void initialize_ports(PORTS_T ** uports)
-{
-    (*uports)->reserved = 1U;
-    (*uports)->port1 = true;
-    (*uports)->port2 = true;
-    (*uports)->port3 = true;
-    (*uports)->port4 = false;
-    (*uports)->port5 = true;
-    (*uports)->port6 = false;
-    (*uports)->port7 = true;
-    (*uports)->port8 = false;
-}
-
-static STATUS_T read_port(uint16_t port, PORTS_T *pports, uint16_t *port_active)
-{
-    STATUS_T result = PASS;
-    int temp_ports = 0U;
-
-    memcpy(&temp_ports, pports, sizeof(temp_ports));
-    printf("%X\n", temp_ports);
-
-    *port_active = (temp_ports & ((uint16_t)1U << (port - 1U)));
-    *port_active = *port_active >> (port - 1U);
-
-    return result;
 }
