30bbe9f6ed391659c120d23b75f833b2b08e7cb1
ivanhmolina <35584318+ivanhmolina@users.noreply.github.com>
2021-09-29 19:40:16 -0500

    ALGORITHMS - SORTING PART 2 (MERGE SORT COMPLETED)
    - mergeSort
    - mergeArrays
    - printArrayln

--- Algorithms/Sorting/inc/sorting.h
+++ Algorithms/Sorting/inc/sorting.h
@@ -2,6 +2,24 @@
 #define _SORTING_
 #include "ptypes.h"
 
+/** 
+ * \brief BUBBLE SORT O(n^2) 
+ *        Optimized version of bubble sort 
+ * \param[in] array
+ * \param[in] length
+ * \return A sorted array
+ * */
 void bubbleSort(int32_t array[], uint16_t length);
+
+/** 
+ * \brief MERGE SORT O(nlog(n))
+ *        Merge algorithm that uses divide and conquer
+ * \pre
+ * \post
+ * \param[in] array
+ * \param[in] length
+ * \retval A sorted array
+ * */
+void mergeSort(int32_t array[], uint16_t length);
 
 #endif
--- Algorithms/Sorting/src/sorting.c
+++ Algorithms/Sorting/src/sorting.c
@@ -3,9 +3,70 @@
 #include "sorting.h"
 
 #define DEBUG_SORT
+/*#define DEBUG_MSORT*/
+
+#ifdef DEBUG_SORT
+static void printArray(int32_t array[], uint16_t length);
+static void printArrayln(int32_t array[], uint16_t length);
+#endif
 
 static void swapInteger(int32_t *a, int32_t *b);
-static void printArray(int32_t array[], uint16_t length);
+static void mergeArrays(int32_t arr1[], uint16_t len1, int32_t arr2[], uint16_t len2);
+
+static void mergeArrays(int32_t arr1[], uint16_t len1, int32_t arr2[], uint16_t len2)
+{
+    uint16_t i = 0;
+    uint16_t j = 0;
+#ifdef DEBUG_MSORT
+    printf("BOTTOM\n");
+    printArray(arr1, len1);
+    printf("   ");
+    printArray(arr2, len2);
+    printf("\n");
+#endif
+    while (i < len1 && j < len2)
+    {
+        if (arr1[i] <= arr2[j])
+        {
+            i++;
+        }
+        if (arr1[i] > arr2[j])
+        {
+            swapInteger(&arr1[i], &arr2[j]);
+            j++;
+        }
+        if (i < len1 && j == len2)
+        {
+            i++;
+        }
+        if (j < len2 && i == len1)
+        {
+            j++;
+        }
+    }
+#ifdef DEBUG_MSORT
+    printf("MERGED\n");
+    printArray(arr1, len1);
+    printf(" + ");
+    printArray(arr2, len2);
+    printf("\n");
+#endif
+}
+
+void mergeSort(int32_t array[], uint16_t length)
+{
+    uint16_t half_len = length / 2;
+#ifdef DEBUG_MSORT
+    printf("ENTERS\n");
+    printArray(array, length);
+#endif
+    if(length > 2)
+    {
+        mergeSort(array, half_len);
+        mergeSort(array + half_len, length - half_len);
+    }
+    mergeArrays(array, half_len, array + half_len, length - half_len);
+}
 
 void bubbleSort(int32_t array[], uint16_t length)
 {
@@ -29,11 +90,26 @@ void bubbleSort(int32_t array[], uint16_t length)
 #ifdef DEBUG_SORT
 int main(int argc, char const *argv[])
 {
-    const uint8_t length = 5;
-    int32_t array[5] = {1, 2, 3, 3, 2};
-    bubbleSort(array, length);
-    printArray(array, length);
-    
+    int32_t array1[5] = {3, 2, 1, 3, 2};
+    int32_t array2[6] = {1, 2, 3, 3, 2, 1};
+    int32_t array3[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+    printf("bubbleSort:\n");
+    printArrayln(array1, 5);
+    bubbleSort(array1, 5);
+    printArrayln(array1, 5);
+    printf("mergeSort:\n");
+    printArrayln(array2, 6);
+    mergeSort(array2, 6);
+    printArrayln(array2, 6);
+    printf("mergeSort:\n");
+    printArrayln(array2, 5);
+    mergeSort(array2, 5);
+    printArrayln(array2, 5);
+    printf("mergeSort:\n");
+    printArrayln(array3, 8);
+    mergeSort(array3, 8);
+    printArrayln(array3, 8);
+
     return 0;
 }
 #endif
@@ -45,11 +121,32 @@ static void swapInteger(int32_t *a, int32_t *b)
     *b = t;
 }
 
+#ifdef DEBUG_SORT
 static void printArray(int32_t array[], uint16_t length)
 {
     uint8_t i;
     printf("[");
-    for (i = 0; i < length - 1u; i++)
-        printf("%d, ", array[i]);
-    printf("%d]\n", array[i]);
+    if(length == 0 || array == NULL)
+        printf("EMPTY]");
+    else
+    {
+        for (i = 0; i < length - 1u; i++)
+            printf("%d, ", array[i]);
+        printf("%d]", array[i]);
+    }
 }
+
+static void printArrayln(int32_t array[], uint16_t length)
+{
+    uint8_t i;
+    printf("[");
+    if (length == 0 || array == NULL)
+        printf("EMPTY]\n");
+    else
+    {
+        for (i = 0; i < length - 1u; i++)
+            printf("%d, ", array[i]);
+        printf("%d]\n", array[i]);
+    }
+}
+#endif
